566b74f8abaef647101533b65dd3ec06927516b0
Zielonyy <zielak134@gmail.com>
2023-01-30 22:30:08 +0100

    fixed

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgqsql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
