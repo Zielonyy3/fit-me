910990df560a1ff18ef5eed5d95000e15978a8c2
Zielonyy <zielak134@gmail.com>
2023-03-21 23:27:46 +0100

    added testing by console

---
+++ app/Console/Commands/TestMediaLibrary.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Exercise;
+use Illuminate\Console\Command;
+
+class TestMediaLibrary extends Command
+{
+    protected $signature = 'test:media-library';
+
+    protected $description = 'Downloads and saves profile pictures in storage/app/profile-pictures.json';
+
+    /**
+     * @throws \ErrorException
+     */
+    public function handle()
+    {
+        $isCreated = false;
+        $exerciseTypes = json_decode(\File::get(resource_path('data/exercises.json')));
+        foreach ($exerciseTypes as $type => $exercises) {
+            foreach ($exercises as $exercise) {
+                if (!$isCreated) {
+                    $this->info('Adding type: ' . $type);
+                    $exercise = Exercise::factory()->create([
+                        'name' => $exercise->name,
+                        'description' => $exercise->instructions,
+                        'owner_id' => null,
+                    ]);
+                    $exercise
+                        ->addMediaFromUrl($this->randomImageUrl())
+                        ->toMediaCollection();
+                    $isCreated = true;
+                }
+            }
+        }
+    }
+}
