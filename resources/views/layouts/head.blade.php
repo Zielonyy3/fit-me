da81c40691cc9bd859b4f16a6da3ba13da63de0e
Zielonyy <zielak134@gmail.com>
2023-03-22 21:11:36 +0100

    added favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -18,6 +18,7 @@
 <link href={{asset('css/tabler.min.css')}} rel="stylesheet">
 <link href={{asset('css/tabler-vendors.min.css')}} rel="stylesheet">
 <link href={{asset('css/select2.min.css')}} rel="stylesheet">
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.ico')}}" />
 
 @stack('styles')
 <style>
